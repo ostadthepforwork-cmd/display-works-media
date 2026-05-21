4f79ec1a9a515c9987aaf02da8b8aef0474a67c8
ostadthepforwork-cmd <info.displayworksmedia@gmail.com>
2026-05-21 13:41:22 +0700

    fix: update keystatic layout

--- src/app/keystatic/layout.tsx
+++ src/app/keystatic/layout.tsx
@@ -1,3 +1,4 @@
-export default function Layout({ children }: { children: React.ReactNode }) {
+import { ReactNode } from 'react';
+export default function Layout({ children }: { children: ReactNode }) {
   return children;
 }
