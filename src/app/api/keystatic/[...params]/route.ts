12d19c7bad2153cf0d7a7e3e1c0472373cd9636a
ostadthepforwork-cmd <info.displayworksmedia@gmail.com>
2026-05-21 13:23:29 +0700

    fix: correct keystatic config import paths

--- src/app/api/keystatic/[...params]/route.ts
+++ src/app/api/keystatic/[...params]/route.ts
@@ -1,3 +1,3 @@
-import { makeRouteHandler } from "@keystatic/next/route-handler";
-import config from "../../../../keystatic.config";
+import { makeRouteHandler } from '@keystatic/next/route-handler';
+import config from '../../../../../keystatic.config';
 export const { POST, GET } = makeRouteHandler({ config });
