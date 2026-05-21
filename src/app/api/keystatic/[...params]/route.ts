ff6ca39cce1da38817128c290a0d5e47756a1ded
ostadthepforwork-cmd <info.displayworksmedia@gmail.com>
2026-05-21 14:48:29 +0700

    fix: explicitly pass env vars to route handler

--- src/app/api/keystatic/[...params]/route.ts
+++ src/app/api/keystatic/[...params]/route.ts
@@ -1,5 +1,9 @@
 import { makeRouteHandler } from '@keystatic/next/route-handler';
 import config from '../../../../../keystatic.config';
 
-const { GET, POST } = makeRouteHandler({ config });
-export { GET, POST };
+export const { GET, POST } = makeRouteHandler({
+  config,
+  clientId: process.env.KEYSTATIC_GITHUB_CLIENT_ID,
+  clientSecret: process.env.KEYSTATIC_GITHUB_CLIENT_SECRET,
+  secret: process.env.KEYSTATIC_SECRET,
+});
