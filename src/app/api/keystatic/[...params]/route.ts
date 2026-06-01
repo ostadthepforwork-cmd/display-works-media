ef25856c2ad56fcec7ce49282de9d571e9de009c
ostadthepforwork-cmd <info.displayworksmedia@gmail.com>
2026-06-01 10:33:03 +0700

    remove keystatic, update components

--- src/app/api/keystatic/[...params]/route.ts
+++
@@ -1,9 +0,0 @@
-import { makeRouteHandler } from '@keystatic/next/route-handler';
-import config from '../../../../../keystatic.config';
-
-export const { GET, POST } = makeRouteHandler({
-  config,
-  clientId: process.env.KEYSTATIC_GITHUB_CLIENT_ID,
-  clientSecret: process.env.KEYSTATIC_GITHUB_CLIENT_SECRET,
-  secret: process.env.KEYSTATIC_SECRET,
-});
